ee728391f460a673f920865d4951754398194f3f
Pati Rocha <patirocha.dev@gmail.com>
2023-04-16 18:48:35 -0300

    update questions.routes.ts

--- src/routes/questions.routes.ts
+++ src/routes/questions.routes.ts
@@ -1,6 +1,19 @@
 import { Router } from "express";
-import { create } from "../controllers/question.controller";
+import { 
+    create, 
+    destroy, 
+    findMany, 
+    findOne,
+    update, } from "../controllers/question.controller";
 
 export const questionsRoutes = Router()
 
-questionsRoutes.post('/questions', create )
+questionsRoutes.post('/questions', create)
+
+questionsRoutes.get('/questions', findMany)
+
+questionsRoutes.get('/questions/:id', findOne)
+
+questionsRoutes.delete('/questions/:id', destroy)
+
+questionsRoutes.put('/questions/:id', update)
