6fbbd1f6b054e1a74a820a9ba3713a458bd74770
Pati Rocha <patirocha.dev@gmail.com>
2023-04-22 00:42:38 -0300

    update add students routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express"; //inicializar express
 import cors from 'cors'
 import { questionsRoutes } from "./routes/questions.routes";
+import { studentsRoutes } from "./routes/students.routes";
 
 export const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(questionsRoutes)
+app.use(studentsRoutes)
